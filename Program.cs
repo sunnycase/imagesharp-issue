4ee722efa0af2cdd0b2b906b1fe9c9a8db3e1a18
sunnycase <sunnycase@live.cn>
2019-02-25 11:27:02 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+
+namespace imagesharp_issue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var image = Image.Load<Rgb24>(typeof(Program).Assembly.GetManifestResourceStream("imagesharp_issue.0.jpg")))
+            {
+                image.Mutate(x =>
+                    x.Resize(128, 128));
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
